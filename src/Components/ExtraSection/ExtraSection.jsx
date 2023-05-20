b2f00d515e05848f1d9c13438b0950c4e0df9230
mdsahil32 <mohammedsahil3211@gmail.com>
2023-05-20 23:42:00 +0600

    deploy firebase

--- src/Components/ExtraSection/ExtraSection.jsx
+++ src/Components/ExtraSection/ExtraSection.jsx
@@ -7,8 +7,19 @@ const ExtraSection = () => {
             <h1 className='text-4xl font-semibold text-center mb-8'>Read About Toys</h1>
             <div className='grid md:grid-cols-2 '>
                 <div>
-                    Hero toys are popular playthings that captivate the imaginations of children and even some adults. These toys are often based on characters from various forms of media, such as movies, comic books, and animated series, and they allow individuals to recreate thrilling adventures and epic battles in their own homes.
-                    Hero toys not only provide entertainment and play value but also offer educational benefits. They encourage storytelling, imagination, and creativity, allowing children to develop cognitive and social skills. Role-playing with hero toys can foster empathy, problem-solving, and collaboration as kids navigate imaginary worlds and create their own narratives.
+                    <p>
+                        Hero toys are a popular category of toys that have captivated the imaginations of children and collectors alike for decades. These toys are inspired by superheroes and characters from various comic books, movies, and animated series. They allow children to recreate epic battles, embark on thrilling adventures, and engage in imaginative play.
+
+                        Hero toys often feature iconic characters like Superman, Batman, Spider-Man, Wonder Woman, Iron Man, and many others. They come in various forms, including action figures, playsets, vehicles, and role-playing accessories. These toys are meticulously designed to resemble their on-screen or comic book counterparts, with detailed costumes, accessories, and even articulation for dynamic posing and playability.
+
+                        The popularity of hero toys goes beyond just children. Many adults are avid collectors and enthusiasts of these toys, appreciating their craftsmanship, nostalgia, and the opportunity to display their favorite characters. Collectible figures and limited-edition releases are highly sought after, often becoming valuable items in the world of toy collecting.
+
+                        Hero toys not only provide entertainment and play value but also offer educational benefits. They encourage storytelling, imagination, and creativity, allowing children to develop cognitive and social skills. Role-playing with hero toys can foster empathy, problem-solving, and collaboration as kids navigate imaginary worlds and create their own narratives.
+
+                        Over the years, hero toys have evolved with advancements in technology. Some toys now incorporate interactive features such as sound effects, lights, and motion sensors, enhancing the overall play experience. Additionally, the representation of diverse characters has become more prominent, reflecting the growing inclusivity in the superhero genre.
+
+                        In summary, hero toys have become an integral part of popular culture, inspiring countless children and adults alike. These toys have a special way of bringing beloved superheroes to life, fostering imagination, and igniting a sense of heroism in the
+                    </p>
                 </div>
                 <div>
                     <img className='h-full w-96' src={img} alt="" />
